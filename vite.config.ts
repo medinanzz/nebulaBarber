c768f872fb22767d627df5b124e3f9b965b8ec6d
Apolo Medina Cshunderlick <apololick2@gmail.com>
2026-05-14 14:30:11 -0300

    configs de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/nebulaBarber",
   server: {
     port: 5000,
     open: true,
   }
-})
+});
